1992f51b8948f82e2bc8bc69eee0c17a413d8c7c
Samuel Grayson <sam@samgrayson.me>
2020-03-25 21:37:31 -0500

    Reworked build infra
    The old build script was lengthy, not easily changeable, difficult to
    read. I rewrote the build scripts into one (hpvm_build.sh) with this
    in mind.
    
    In the past, everything in llvm_patches gets placed at the same relative path in
    llvm EXCEPT include headers. I changed this to be more consistent, not
    hard-code paths, and make it easier to add a new file to llvm_patches
    in the future (it will be automatically picked up).
    
    I added infrastructure to build in a docker container. I use a docker
    script instead of docker container, so the process in the container
    can adopt the user is running on the host system. Now the container
    can write to the volume as the real user--not as root.

---
+++ hpvm/scripts/run_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e -x
+cd "$(dirname "${0}")/.."
+
+env \
+	be_user=yes \
+	mount_cwd=yes \
+	interactive=yes \
+	packages="git wget xz-utils cmake make g++ python ocl-icd-opencl-dev" \
+	command="./scripts/hpvm_build.sh" \
+	./scripts/docker.sh
