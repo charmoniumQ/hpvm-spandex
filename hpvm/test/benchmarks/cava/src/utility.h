2e3f229fa271b3b5508fee63434b6de79962d6ff
Samuel Grayson <sam@samgrayson.me>
2020-12-18 14:32:38 -0600

    Removed hpvm tests

--- hpvm/test/benchmarks/cava/src/utility.h
+++
@@ -1,8 +0,0 @@
-#ifndef _COMMON_UTILITY_H_
-#define _COMMON_UTILITY_H_
-
-#include <stddef.h>
-
-void *malloc_aligned(size_t size);
-
-#endif
--- hpvm/test/benchmarks/parboil/benchmarks/spmv/common_src/convert-dataset/commontools.mk
+++
@@ -1,2 +0,0 @@
-
-SRCDIR_OBJS += ../../common_src/convert-dataset/convert_dataset.ll ../../common_src/convert-dataset/mmio.ll
