47d88b084b7d1baa6aa16f027b5f0881033f1e87
Samuel Grayson <sam@samgrayson.me>
2020-12-19 17:56:06 -0600

    fix scripts

--- hpvm/scripts/run_docker.sh
+++ hpvm/scripts/run_docker.sh
@@ -2,6 +2,11 @@
 set -e -x
 
 hpvm="$(realpath "$(dirname "${0}")/..")"
+if [ -n "${1}" ]; then
+	next_command="${*}"
+else
+	next_command="/bin/bash"
+fi
 
 env \
 	be_user=yes \
@@ -9,7 +14,7 @@ env \
 	os_tag=20.04 \
 	mount_cwd=yes \
 	interactive=yes \
-	sameplace_mounts="${hpvm} ${real_pwd}" \
+	sameplace_mounts="${hpvm}" \
 	packages="git wget xz-utils cmake make g++ python ocl-icd-opencl-dev software-properties-common ninja-build" \
-	command="sh -c '${hpvm}/scripts/hpvm_build.sh && ${*}'" \
+	command="sh -c '${hpvm}/scripts/hpvm_build.sh && ${next_command}'" \
 	"${hpvm}/scripts/docker.sh"
